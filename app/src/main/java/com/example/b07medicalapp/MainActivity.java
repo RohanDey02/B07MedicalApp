dce12fe7a44a25ba76cf8fc3b9a004cad35dfc70
RohanDey02 <58871401+RohanDey02@users.noreply.github.com>
2021-07-28 14:07:14 -0400

    Completed Main Page UI
    Completed the Main Page UI. Made a custom background using Photoshop and then rearranged icons, colours and layouts..

--- app/src/main/java/com/example/b07medicalapp/MainActivity.java
+++ app/src/main/java/com/example/b07medicalapp/MainActivity.java
@@ -9,6 +9,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_main);
     }
 }
