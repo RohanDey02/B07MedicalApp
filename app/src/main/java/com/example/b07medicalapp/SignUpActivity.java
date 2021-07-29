f8722658b20bf231cfe2c5122389c8eceda67023
Oliiiiv <weng.zhang0708@gmail.com>
2021-07-29 21:13:32 +0800

    Sign up page
    perhaps there are some problems with strings file. I have no idea why this happens :::(

---
+++ app/src/main/java/com/example/b07medicalapp/SignUpActivity.java
@@ -0,0 +1,15 @@
+package com.example.b07medicalapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+}
