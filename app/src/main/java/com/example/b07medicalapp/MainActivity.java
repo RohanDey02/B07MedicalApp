04837c3f2c1056c55ccbd86c356aacc762746086
RohanDey02 <58871401+RohanDey02@users.noreply.github.com>
2021-07-30 19:45:09 -0400

    Connected Main Page and Signup Page
    Connected the main page to the signup page and made a minor change to the UI of the main page. Also connected signup page with main page.

--- app/src/main/java/com/example/b07medicalapp/MainActivity.java
+++ app/src/main/java/com/example/b07medicalapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.b07medicalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.firebase.database.DatabaseReference;
 
@@ -13,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
     }
+
+    // This method will be called on clicking the Register button on the main page.
+    public void mainRegister(View view){
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/b07medicalapp/SignUpActivity.java
+++ app/src/main/java/com/example/b07medicalapp/SignUpActivity.java
@@ -1,15 +1,29 @@
 package com.example.b07medicalapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
 public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_sign_up);
+
+        Intent intent = getIntent();
     }
 
+    // This method will be called on clicking the Register button on the signup page.
+    public void signUpRegister(View view) throws InterruptedException {
+        Intent intent = new Intent(this, MainActivity.class);
+
+        startActivity(intent);
+    }
 }
