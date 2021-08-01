843061499a987e920df81df1966054a21fb050d7
Yuval Kamani <yuvalkamani@gmail.com>
2021-08-02 01:19:41 +0530

    Added the list of doctors in the database to the book appointment page
    Read values from the child of "doctors" in the database to display the doctors' names in the book appointments page.

--- app/src/main/java/com/example/b07medicalapp/BookAppointmentActivity.java
+++ app/src/main/java/com/example/b07medicalapp/BookAppointmentActivity.java
@@ -4,7 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class BookAppointmentActivity extends AppCompatActivity {
 
@@ -14,5 +21,30 @@ public class BookAppointmentActivity extends AppCompatActivity {
         setContentView(R.layout.activity_book_appointment);
 
         Intent intent = getIntent();
+
+        DatabaseReference ref = FirebaseDatabase.getInstance("https://b07projectdatabase-default-rtdb.firebaseio.com/").getReference("doctors");
+        ValueEventListener listener = new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                int[] textField = {R.id.bk1, R.id.bk2, R.id.bk3, R.id.bk4, R.id.bk5,
+                        R.id.bk6, R.id.bk7, R.id.bk8, R.id.bk9, R.id.bk10};
+                int i = 0;
+                for (DataSnapshot child : dataSnapshot.getChildren()){
+                    Doctor doctor = child.getValue(Doctor.class);
+                    Log.i("doc info:", doctor.toString());
+
+                    TextView textView = (TextView) findViewById(textField[i]);
+                    i++;
+                    textView.append(doctor.firstName);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("warning", "onCancelled", databaseError.toException());
+            }
+        };
+        ref.addValueEventListener(listener);
     }
 }
