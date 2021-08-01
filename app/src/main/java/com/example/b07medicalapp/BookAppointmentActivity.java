ccd1c077b51521d9d5f150c041a39e911870ac3d
Yuval Kamani <yuvalkamani@gmail.com>
2021-08-01 23:57:08 +0530

    Updated the 'Book' button in ListAppointment
    Created an intent on the 'Book' button in the ListAppointment class so that it goes to the BookAppointment activity page

--- app/src/main/java/com/example/b07medicalapp/BookAppointmentActivity.java
+++ app/src/main/java/com/example/b07medicalapp/BookAppointmentActivity.java
@@ -2,7 +2,9 @@ package com.example.b07medicalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class BookAppointmentActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class BookAppointmentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_appointment);
+
+        Intent intent = getIntent();
     }
 }
--- app/src/main/java/com/example/b07medicalapp/ListAppointment.java
+++ app/src/main/java/com/example/b07medicalapp/ListAppointment.java
@@ -2,7 +2,9 @@ package com.example.b07medicalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ListAppointment extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class ListAppointment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_appointment);
+
+        Intent intent = getIntent();
+    }
+
+    //open the book appointment activity page when 'Book' is pressed
+    public void openBookAppointmentPage(View view) {
+        Intent intent = new Intent(this, BookAppointmentActivity.class);
+        startActivity(intent);
     }
 }
