8607e60e5fdc8648e2f93303db55e5a0c96222ad
Yuval Kamani <yuvalkamani@gmail.com>
2021-08-03 21:01:36 +0530

    Added Appointments Available to the Book Appointment Page
    Added spinners to specify the availability of each doctor as a drop down menu.

--- app/src/main/java/com/example/b07medicalapp/BookAppointmentActivity.java
+++ app/src/main/java/com/example/b07medicalapp/BookAppointmentActivity.java
@@ -2,10 +2,11 @@ package com.example.b07medicalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -13,6 +14,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class BookAppointmentActivity extends AppCompatActivity {
     private View decorView;
@@ -41,14 +45,29 @@ public class BookAppointmentActivity extends AppCompatActivity {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 int[] textField = {R.id.bk1, R.id.bk2, R.id.bk3, R.id.bk4, R.id.bk5,
                         R.id.bk6, R.id.bk7, R.id.bk8, R.id.bk9, R.id.bk10};
+
+                int[] spinnerField = {R.id.s1, R.id.s2, R.id.s3, R.id.s4, R.id.s5,
+                        R.id.s6, R.id.s7, R.id.s8, R.id.s9, R.id.s10};
+
                 int i = 0;
                 for (DataSnapshot child : dataSnapshot.getChildren()){
                     Doctor doctor = child.getValue(Doctor.class);
                     Log.i("doc info:", doctor.toString());
 
                     TextView textView = (TextView) findViewById(textField[i]);
-                    i++;
                     textView.setText(doctor.firstName + " " + doctor.lastName.charAt(0) + ".");
+
+                    Spinner spinner = (Spinner) findViewById(spinnerField[i]);
+                    Map<String, String> availability = doctor.getAvailability();
+                    ArrayList<String> dates = new ArrayList<>();
+                    for (Map.Entry<String, String> entry : availability.entrySet()) {
+                        dates.add(entry.getKey());
+                    }
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(BookAppointmentActivity.this, android.R.layout.simple_spinner_item, dates);
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    spinner.setAdapter(adapter);
+
+                    i++;
                 }
             }
 
